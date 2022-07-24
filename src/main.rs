60d1849a3a2bd78c899a22d8a9e65f2b8a3394b8
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-07-25 00:09:41 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate args;
+extern crate getopts;
+
+use chess::{All_RANKS, Board, BoardStatus, ChessMove, Color, MobveGen, Piece};
+//체스?/
+use std::env;
+use std::io::{self, BufRead};
+
+const STARTING_FEN: &str;
 fn main() {
     println!("체스둘거임");
 }
