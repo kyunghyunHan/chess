87ea2b98d09e378097bd38947250cffa78bac0f3
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-07-25 22:24:42 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,31 @@ const PROGRAM_NAME: &'static str = "Amano";
 
 fn calc_piece_value(pc_inx: usize, sq_idx: usize, color: Option<Color>) -> i64 {
     match color {
-
-        Some
+        Some(Color::White) => {
+            let sq_value = piecesValues::PIECE_SQUARES[pc_inx][sq_idx];
+            return -(piecesValues::PIECE_VALS[pc_inx] + sq_value);
+        }
+        Some(Color::Black) => {
+            let sq_value = piecesValues::PIECE_SQUARES[pc_inx][63 - sq_idx];
+            return piecesValues::PIECE_VALS[pc_inx] + sq_value;
+        }
+        None => {
+            return 0;
+        }
     }
+}
+fn calc_pieces_value(board: &Board) -> i64 {
+    let mut result = 0;
+    for pc_idx in 0..6 {
+        let pc_type = piecesValues::PIECES[pc_idx];
+        let bboard = *board.pieces(pc_type);
+        for square in bboard {
+            let sq_idx = square.to_index();
+            result += calc_piece_value(pc_idx, sq_idx, board.color_on(square));
+        }
+    }
+    result
+}
+fn main() {
+    println!("hello");
 }
