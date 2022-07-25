b1f9b228b5b9742f4c228a4c81878584a69a6551
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-07-25 10:13:01 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate args;
 extern crate getopts;
 
-use chess::{All_RANKS, Board, BoardStatus, ChessMove, Color, MobveGen, Piece};
-//체스?/
+use args::{Args, ArgsError};
+use chess::{get_rank, Board, BoardStatus, ChessMove, Color, MoveGen, Piece, ALL_RANKS};
+use colored::{ColoredString, Colorize};
+use getopts::Occur;
 use std::env;
 use std::io::{self, BufRead};
+use std::str::FromStr;
+use std::time::{Duration, Instant};
+mod benchmarks;
+mod piecesValues;
 
-const STARTING_FEN: &str;
-fn main() {
-    println!("체스둘거임");
-}
+const STARTING_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+const TEST_FEN: &str = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1";
+const DEFAULT_DEPTH: i64 = 7;
+
+const PROGRAM_DESC: &'static str = "A good old fashioned Rust chess engine";
+const PROGRAM_NAME: &'static str = "Amano";
